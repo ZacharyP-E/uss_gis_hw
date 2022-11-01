8c65d66fa40c00f335305fc7ed59d0bb7fc2316e
ZacharyP-E <zacharypescodeveriss@gmail.com>
2022-11-01 16:52:47 +0000

    progress on code to read csv, and to join. Need to convert country codes to be the same

--- gis_hw_wk4.R
+++ gis_hw_wk4.R
@@ -1,11 +1,39 @@
 library(tidyverse)
-library(readxl)
 library(here)
+library(sf)
+library(countrycode)
 
-#Identifying the local path in which the excel data is located
-xl_path = "data/HDR21-22_Statistical_Annex_GII_Table.xlsx"
+#Identifying the local path in which the data is located
+csv_path <- "data/HDR21-22_Composite_indices_complete_time_series.csv"
+shp_path <- "data/World_Countries_(Generalized)/World_Countries__Generalized_.shp"
 
-sheet_x <- excel_sheets(here(xl_path))[1] #Identifying the sheet in which the data is housed. 
-df <- read_excel(here(xl_path), sheet = sheet_x, skip=7) #Reading in the excel data from the appropriate sheet.
+#Reading in the csv file
+df <- read_csv(here(csv_path))
 
-print("hello")
+#Identfiying the columns to be used in analysis
+df1 <- df %>%
+  select(c("iso3", "country", "hdi_2010", "hdi_2019"))
+
+#Calcualting the difference between 2010 and 2019
+df2 <- df1 %>%
+  mutate("difference"= hdi_2019 - hdi_2010)
+
+#Reading in the shapefile
+shape <- read_sf(shp_path)
+
+#Convert country codes to be in same format using countrycode package
+#Documentation: https://www.rdocumentation.org/packages/countrycode/versions/1.4.0/topics/countrycode
+
+
+
+#####Incorrect code
+#Merging the shapefule and the csv together
+df_m <- shape %>%
+  inner_join(., 
+            df2,
+            by = c("COUNTRY" = "country"))
+
+
+
+
+
